0c49ac5cd083e25fa59c131a9415a99f001caf23
jjrosey <julian.rosenthal@att.net>
2018-04-10 20:52:39 -0700

    add broken script; add error checking

--- find_and_compile_au3s.py
+++ find_and_compile_au3s.py
@@ -1,5 +1,6 @@
 import os
 import fnmatch
+import subprocess
 
 def find_au3s():
    return [os.path.join(dirpath, f)
@@ -11,9 +12,15 @@ def invoke_Aut2Exe(au3s):
       name = os.path.splitext(au3)[0]
       if os.path.exists(name + ".exe"):
          os.remove(name + ".exe") 
-      cmd = "wine /local/workspace/jenkins/Aut2exe.exe /in " + name + ".au3 /out " + name + ".exe"
+      cmd = "wine ./Aut2exe.exe /in " + name + ".au3 /out " + name + ".exe"
       print ("cmd: %s" % cmd)
-      os.system(cmd)
+      process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+      output, error = process.communicate()
+      if process.returncode != 0:
+         print ("output: %s" % output)
+         print ("error: %s" % error)
+      else:
+         print("cmd completed!")
    
 
 if  __name__ == "__main__":
